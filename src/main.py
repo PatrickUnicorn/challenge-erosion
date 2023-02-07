a8f8fe98e96ea76809e08499725affaebffbffea
PatrickUnicorn <patrick.alves03@hotmail.com>
2023-02-07 15:33:17 +0000

    change

---
+++ src/main.py
@@ -0,0 +1,24 @@
+def img2ascii(img_data: list[list[int]]) -> str:
+    black = '#'
+    white = '.'
+    return '\n'.join(
+        ''.join(black if pixel else white for pixel in row)
+        for row in img_data
+    )
+
+img = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+ascii = img2ascii(img)
+print(repr(ascii))
+print(ascii)
