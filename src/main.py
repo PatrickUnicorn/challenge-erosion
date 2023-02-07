cf019ae145d3d9cc2c632f927cd3495dffa98062
PatrickUnicorn <patrick.alves03@hotmail.com>
2023-02-07 15:38:27 +0000

    problème 1 fonctionne

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-def img2ascii(img_data: list[list[int]]) -> str:
-    black = '#'
-    white = '.'
+def img2ascii(img_data: list[list[int]], black:str = '#', white:str = '.' ) -> str:
     return '\n'.join(
         ''.join(black if pixel else white for pixel in row)
         for row in img_data
